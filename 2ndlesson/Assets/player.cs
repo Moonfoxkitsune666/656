7542aefdb9d9ffbf5a5825ca6925b2e25744f282
Moonfoxkitsune666 <73173359+Moonfoxkitsune666@users.noreply.github.com>
2020-10-27 15:23:15 +0100

    laser defender
    part 1
    part 2

---
+++ 2ndlesson/Assets/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+
+        var newXPos = transform.position.x + deltaX;
+
+        this.transform.position = new Vector2(newXPos, CryptoAPITransform.position.y);
+    }
+}
