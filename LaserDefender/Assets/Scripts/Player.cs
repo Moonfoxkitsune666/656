b7c0dc85984ab3cd48429717c8b70257758fc83c
Moonfoxkitsune666 <73173359+Moonfoxkitsune666@users.noreply.github.com>
2020-11-03 13:34:28 +0100

    66
    ok

---
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //var is a generic variable which changes its type according to volue
+        //deltaX is the difference the Player Ship moves in the x-axis 
+        var deltaX = Input.GetAxis("Horizontal");
+
+        //newXPosition = current pos in x + difference moved in x-axis
+        var newXPos = transform.position.x + deltaX;
+
+        //move the Player Ship on the x-axis only (newXPos)
+        this.transform.position = new Vector2(newXPos, transform.position.y);
+
+
+    }
+}
